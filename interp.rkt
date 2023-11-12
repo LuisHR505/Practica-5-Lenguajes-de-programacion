cddd3c86909cc92e12c56bc5bdc8ca0792b3b9c2
Julian Rosas <julianrosas11032002@ciencias.unam.mx>
2023-11-12 15:03:41 -0600

    solo falta rec

--- interp.rkt
+++ interp.rkt
@@ -18,7 +18,45 @@
   [rec-cons-env (id symbol?) (value boxed-RCFSBAE-Val?) (rest-env Env?)])
 
 ;; RCFSBAE x Env -> RCFSBAE-Val
-(define (interp expr env) (error 'interp "Sin implementar"))
+(define (interp expr env)
+  (type-case RCFSBAE expr
+    [num (n) (num-v n)]
+    [id (i) (lookup id env)]
+    [bool (b) (bool-v b)]
+    [strinG (s) (string-v s)]
+    [op (f args) (interp (parse (apply f (greedy-eval (map (lambda (e) (interp e env)) args)))) mt-env)]
+    [fun (param body) (closure-v param body env)]
+    [app (fun-e args)(let ([fun-val (interp fun-e env)])
+                         (interp (closure-v-body fun-val)
+                                 (closure-params (closure-v-args fun-val) args (closure-v-env fun-val))))]
+    [iF (test-e then-e else-e)
+        (type-case RCFSBAE-Val (interp test-e env)
+          [bool-v (b)
+                  (if b
+                      (interp then-e env)
+                      (interp else-e env))]
+          [else (error 'interp "La condicion del if debe ser un booleano")])]
+    [rec (bindings body) (interp (rec->aux bindings body))]))
+
+
+
+;; closure-params :: symbol x args x Env -> Env
+(define (closure-params vars args env)
+  (if (equal? (length vars) (length args))
+      (if (empty? vars)
+          env
+          (cons-env (car vars) (interp (car args) env) (closure-params (cdr vars) (cdr args) env)))
+      (error 'interp "Numero de argumentos y parametros distinto")))
+
+;;greedy-eval :: CFSBAE-Val -> Val
+(define (greedy-eval args)
+  (if (empty? args)
+      '()
+      (type-case RCFSBAE-Val (car args)
+        [num-v (n) (cons n (greedy-eval (cdr args)))]
+        [bool-v (b) (cons b (greedy-eval (cdr args)))]
+        [string-v (s) (cons s (greedy-eval (cdr args)))]
+        [closure-v (args body env) error 'interp "error de sintaxis"] )))
 
 ;; symbol x Env -> RCFSBAE-Val
 
